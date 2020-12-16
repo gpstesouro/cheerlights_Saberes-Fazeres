e6cb0984061831978dccf7c230c87a1fbe49e087
Mario Câmara <60160062+Mario-Camara@users.noreply.github.com>
2020-12-16 13:51:09 -0300

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,11 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "***********"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "**********"		// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID_COLOR 1417	          	//MathWorks color cheerlights
+
+//MQTT - Mosquitto (em construção)
+//#define SECRET_****** 298725 				              //Test channel for counting
+//#define SECRET_****** "ABCDEFGHIJLMNOPQRST"    		//API Key for Test channel
