01e7662f5f21e2c093f95d137ac3ec5b8b696104
Mario Câmara <60160062+Mario-Camara@users.noreply.github.com>
2020-12-29 23:07:38 -0300

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -4,8 +4,11 @@
 #define SECRET_SSID "***********"		// replace MySSID with your WiFi network name
 #define SECRET_PASS "**********"		// replace MyPassword with your WiFi password
 
-#define SECRET_CH_ID_COLOR 1417	          	//MathWorks color cheerlights
+//Broker MQTT - Mosquitto
+#define ADDRESS_MQTTSERVER "67.225.227.165" // MQTT Broker OFICIAL "mqtt.cheerlights.com" (67.225.227.165:1883) // broker desabilitado a fazer outros Publisher.
+//#define ADDRESS_MQTTSERVER "5.196.95.208"     // MQTT Broker "test.mosquitto.org" (5.196.95.208:1883) // aconteceu de insistir (erroneamente) em voltar para a cor blue.
 
-//MQTT - Mosquitto (em construção)
-//#define SECRET_****** 298725 				              //Test channel for counting
-//#define SECRET_****** "ABCDEFGHIJLMNOPQRST"    		//API Key for Test channel
+//MathWorks-ThingSpeak:
+#define ID_CHANNEL 1270120                     //cheerlights_saberes-afazeres
+#define SECRET_READAPIKEY "76DP3IOOV6ECRTCO"	 //API Key for Test channel
+#define SECRET_WRITEAPIKEY "698HWRJZTNEEX2P8"  //API Key for Test channel
